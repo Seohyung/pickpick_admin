c81e707d1aa4cff769a41e3326ac69d42af8f4cd
chani0520 <kimchan0765@gmail.com>
2022-03-11 13:54:13 +0900

    질문 & 유저 정보 삭제 로직 수정

--- src/questions.js
+++ src/questions.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { List, Datagrid, TextField, ImageField, DateField } from 'react-admin';
+import PostPagination from './PostPagination';
 
 export const QuestionList = (props) => (
-  <List {...props}>
+  <List {...props} pagination={<PostPagination />}>
     <Datagrid rowClick='edit'>
       <TextField source='id' />
       <TextField source='author.nickname' />
--- src/users.js
+++ src/users.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { List, Datagrid, TextField, DateField, EmailField } from 'react-admin';
+import PostPagination from './PostPagination';
 
 export const UserList = (props) => (
-  <List {...props}>
+  <List {...props} pagination={<PostPagination />}>
     <Datagrid rowClick='edit'>
       <TextField source='id' />
       <TextField source='nickname' />
